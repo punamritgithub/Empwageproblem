2152bf1751d65058254cb036e772143e2f7eedb7
punamr <punamjondhale1994@gmail.com>
2022-09-29 21:49:32 -0700

    Added:Emp part time &full time

--- src/com/brigelabz/Empwage.java
+++ src/com/brigelabz/Empwage.java
@@ -1,22 +1,37 @@
 package com.brigelabz;
 
 public class Empwage {
-    public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
-        int Employee_wage_per_Hour = 20;
-        int empHrs = 0;
-        int empwage = 0;
-        double empcheck = Math.floor(Math.random() * 10) % 2;
-        if (empcheck == IS_FULL_TIME)
-            empHrs = 8;
-        else
-            empHrs = 0;
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_TIME_HOUR = 8;
+    static final int PART_TIME_HOUR = 4;
 
 
-        empwage = empHrs * Employee_wage_per_Hour;
-        System.out.println("empwage: " + empwage);
+
+    public static void main(String[]args) {
+        double var1 = Math.random();
+        System.out.println(var1);
+        double var = Math.floor(var1 * 10);
+        System.out.println(var);
+        int employeeAttendence = (int) var % 3;
+        System.out.println(employeeAttendence);
+        int dailyWage = 0;
+        if (employeeAttendence == 2) {
+            System.out.println("Employee is Full time");
+            dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+        } else if (employeeAttendence == 1) {
+            System.out.println("Employee is Part time");
+            dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+        } else {
+            System.out.println("Employee is Absent");
+        }
+        System.out.println("Daily Wage => " + dailyWage);
     }
 }
+
+
+
+
+
 
 
 
