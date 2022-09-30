a5ec09a31f4892e345a1b8ceea1b936d7042f38f
punamr <punamjondhale1994@gmail.com>
2022-09-29 21:27:11 -0700

    Added:Emp Attendance check

--- src/com/brigelabz/Empwage.java
+++ src/com/brigelabz/Empwage.java
@@ -1,9 +1,24 @@
 package com.brigelabz;
 
 public class Empwage {
-    public static void main(String[] args) {
+    public static void EmployeepresentAbsent() {
+        double var1 = Math.random();
+        System.out.println(var1);
+        double var = Math.floor(var1 * 10);
+        System.out.println(var);
+        int employeeAttendence = (int) var % 2;
+        System.out.println(employeeAttendence);
+        if (employeeAttendence == 1) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is Abesent");
+        }
 
-        System.out.println("Welcome to the Employee wage program");
+    }
+
+    public static void main(String[] arg) {
+        EmployeepresentAbsent();
+
     }
 }
 
