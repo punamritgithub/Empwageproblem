cd1b1ef713e9744ca1c78a8ae9b09c0b2c15ce06
punamr <punamjondhale1994@gmail.com>
2022-09-29 21:22:09 -0700

    Added: Welcome to the empolyee wage problem

---
+++ src/com/brigelabz/Empwage.java
@@ -0,0 +1,22 @@
+package com.brigelabz;
+
+public class Empwage {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Employee wage program");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
