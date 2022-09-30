8bfaa0040ab5b858088553e4490b86acdd30db0f
punamr <punamjondhale1994@gmail.com>
2022-09-29 22:13:01 -0700

    Added:Calculatation Emp month wage

--- src/com/brigelabz/Empwage.java
+++ src/com/brigelabz/Empwage.java
@@ -1,37 +1,40 @@
 package com.brigelabz;
 
 public class Empwage {
-    static final int IS_FULL_TIME = 2;
-    static final int IS_PART_TIME = 1;
+    public static final int is_full_time = 2;
+    public static final int is_part_time=1;
+    public static final int Employee_wage_per_Hour = 20;
+    public static final int number_of_working_day = 20;
 
-    static final int EMP_RATE_PER_HRS = 20;
-
-    public static void main(String[] arg) {
+    public static void main(String[] args) {
 
         int emphrs = 0;
         int empwage = 0;
-        int employeeAttendance = (int) Math.floor(Math.random() * 10) % 3;
-        System.out.println("employeeAttendance");
 
-        if (employeeAttendance == 2) {
+
+        int totalempwage = 0;
+
+        for (int day = 0; day < number_of_working_day; day++) {
+            int employeeAttendance = (int) Math.floor(Math.random() * 10) % 3;
             switch (employeeAttendance) {
-                case IS_FULL_TIME:
+
+                case is_full_time:
                     System.out.println("Employee is full time");
                     emphrs = 8;
                     break;
-                case IS_PART_TIME:
+                case is_part_time:
                     System.out.println("Employee is part time");
                     emphrs = 4;
                     break;
                 default:
-                    System.out.println("Employee is absent");
-
-
+                    emphrs = 0;
+                    break;
             }
-            empwage = emphrs * EMP_RATE_PER_HRS;
+            empwage = emphrs * Employee_wage_per_Hour;
             System.out.println("empwage: " + empwage);
 
         }
+        System.out.println("Total Emp Wage: " + totalempwage);
     }
 }
 
