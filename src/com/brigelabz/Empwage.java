d64d672a7ff74a319acb1f08163c7d577033b4e6
punamr <punamjondhale1994@gmail.com>
2022-09-29 22:00:41 -0700

    Added:Calculated Empwage using switch

--- src/com/brigelabz/Empwage.java
+++ src/com/brigelabz/Empwage.java
@@ -1,32 +1,43 @@
 package com.brigelabz;
 
 public class Empwage {
-    static final int WAGE_PER_HOUR = 20;
-    static final int FULL_TIME_HOUR = 8;
-    static final int PART_TIME_HOUR = 4;
+    static final int IS_FULL_TIME = 2;
+    static final int IS_PART_TIME = 1;
+
+    static final int EMP_RATE_PER_HRS = 20;
+
+    public static void main(String[] arg) {
+
+        int emphrs = 0;
+        int empwage = 0;
+        int employeeAttendance = (int) Math.floor(Math.random() * 10) % 3;
+        System.out.println("employeeAttendance");
+
+        if (employeeAttendance == 2) {
+            switch (employeeAttendance) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee is full time");
+                    emphrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee is part time");
+                    emphrs = 4;
+                    break;
+                default:
+                    System.out.println("Employee is absent");
 
 
+            }
+            empwage = emphrs * EMP_RATE_PER_HRS;
+            System.out.println("empwage: " + empwage);
 
-    public static void main(String[]args) {
-        double var1 = Math.random();
-        System.out.println(var1);
-        double var = Math.floor(var1 * 10);
-        System.out.println(var);
-        int employeeAttendence = (int) var % 3;
-        System.out.println(employeeAttendence);
-        int dailyWage = 0;
-        if (employeeAttendence == 2) {
-            System.out.println("Employee is Full time");
-            dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
-        } else if (employeeAttendence == 1) {
-            System.out.println("Employee is Part time");
-            dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-        } else {
-            System.out.println("Employee is Absent");
         }
-        System.out.println("Daily Wage => " + dailyWage);
     }
 }
+
+
+
+
 
 
 
